ef66c5553b46e88ab5d6097aba9133cc55e275d3
Lutz Häußler <lutz.haeussler@coronic.de>
2018-04-04 01:06:29 +0200

    add: first successful opengl build

--- GeodeticFloes/main.cpp
+++ GeodeticFloes/main.cpp
@@ -1,26 +1,61 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 
 #include <windows.h>
+#include <GL/glew.h>
+
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) 
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// create the window
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+	window.setVerticalSyncEnabled(true);
 
-	while (window.isOpen())
+	// activate the window
+	window.setActive(true);
+
+	// load resources, initialize the OpenGL states, ...
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
 	{
+		std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+	}
+
+	std::cout << "no output" << std::endl;
+	std::cerr << "no error" << std::endl;
+
+	// run the main loop
+	bool running = true;
+	while (running)
+	{
+		// handle events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
+			{
+				// end the program
+				running = false;
+			}
+			else if (event.type == sf::Event::Resized)
+			{
+				// adjust the viewport when the window is resized
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
 		}
 
-		window.clear();
-		window.draw(shape);
+		// clear the buffers
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// draw...
+
+		// end the current frame (internally swaps the front and back buffers)
 		window.display();
 	}
 
+	// release resources...
+
 	return 0;
-}
+} 
