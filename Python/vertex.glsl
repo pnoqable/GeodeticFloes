15e5357916cd47aafebabc8d531237e5bbd41351
Lutz Häußler <lutz.haeussler@web.de>
2020-04-09 20:02:55 +0200

    add: fog and extra transparency in vertex shader

--- Python/vertex.glsl
+++ Python/vertex.glsl
@@ -9,4 +9,9 @@ void main()
 {
     gl_Position = mvp * gl_Vertex;
     gl_FrontColor = gl_Color;
+
+    float fogSwitch = clamp( 4. * mvp[3].z - 3.5, 0., 1. );
+    float fogValue = smoothstep( mvp[3].z - .3, mvp[3].z - .1, gl_Position.z );
+    float insight = clamp( 4. * gl_Position.z, 0., 1. );
+    gl_FrontColor.a *= insight * ( 1. - .75 * fogSwitch * fogValue );
 }
